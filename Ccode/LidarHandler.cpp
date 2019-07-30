35cc1fcd203a1a5bc41715eb4ed2cfd4ad618bd5
Kotan Tamas <33610984+torta211@users.noreply.github.com>
2019-07-30 13:03:45 +0200

    working frame by frame visu

--- Ccode/LidarHandler.cpp
+++ Ccode/LidarHandler.cpp
@@ -4,45 +4,49 @@
 #include <iostream>
 #include <Windows.h>
 
+PyObject* get_a_frame()
+{
+	VelodyneVLP16PCAP capture;
+	capture.open_live();
 
-PyObject* get_a_test_list() {
-	std::cout << "Converting an std::vector to Python vector" << std::endl;
-
-	PyObject* result_list = PyList_New(0);
-
-	for (int i = 0; i < 100; i++)
+	std::vector<DataPoint> dataPoints;
+	while (capture.isRun())
 	{
-		PyObject* sub_list = PyList_New(0);
-		PyList_Append(sub_list, PyFloat_FromDouble(i));
-		PyList_Append(sub_list, PyFloat_FromDouble(i * 2.0 + i));
-		PyList_Append(sub_list, PyFloat_FromDouble(i * 0.5 - i));
+		capture.retrieve(dataPoints);
+		if (dataPoints.empty())
+		{
+			continue;
+		}
+		else
+		{
+			break;
+		}
 
-		PyList_Append(result_list, sub_list);
 	}
-	return result_list;
-}
 
-const double e = 2.7182818284590452353602874713527;
+	std::cout << "frame read in c++ vector form" << std::endl;
+	std::cout << "number of points in this frame: " << dataPoints.size() << std::endl;
 
-double sinh_impl(double x) {
-	return (1 - pow(e, (-2 * x))) / (2 * pow(e, -x));
-}
+	PyObject* frame_as_list = PyList_New(0);
 
-double cosh_impl(double x) {
-	return (1 + pow(e, (-2 * x))) / (2 * pow(e, -x));
-}
+	for (const DataPoint& laser : dataPoints)
+	{
+		PyObject* one_point = PyList_New(0);
+		PyList_Append(one_point, PyFloat_FromDouble(laser.coordinates.x));
+		PyList_Append(one_point, PyFloat_FromDouble(laser.coordinates.y));
+		PyList_Append(one_point, PyFloat_FromDouble(laser.coordinates.z));
 
-PyObject* tanh_impl(PyObject*, PyObject* o) {
-	double x = PyFloat_AsDouble(o);
-	double tanh_x = sinh_impl(x) / cosh_impl(x);
-	return PyFloat_FromDouble(tanh_x);
+		PyList_Append(frame_as_list, one_point);
+	}
+
+	std::cout << "passing frame to python code" << std::endl;
+	return frame_as_list;
 }
 
 static PyMethodDef lidarhandler_methods[] = {
 	// The first property is the name exposed to Python, fast_tanh, the second is the C++
 	// function name that contains the implementation.
-	{ "fast_tanh", (PyCFunction)tanh_impl, METH_O, nullptr },
-    { "get_test_list", (PyCFunction)get_a_test_list, METH_O, nullptr },
+    { "get_frame", (PyCFunction)get_a_frame, METH_O, nullptr },
 
 	// Terminate the array with an object containing nulls.
 	{ nullptr, nullptr, 0, nullptr }
--- LidarWorkingSolution/LidarWorkingSolution.py
+++ LidarWorkingSolution/LidarWorkingSolution.py
@@ -1,12 +1,18 @@
-from Ccode import get_test_list
+from Ccode import get_frame
 import numpy as np
 import open3d as o3d
 
+for i in range(0, 1000):
+    frame = get_frame(10);
+    print("frame arrived to Python")
 
-a = get_test_list(10);
+    frame_in_np = np.array(frame)
+    print(frame_in_np.shape)
 
-pcl_to_disp = np.array(a)
+    pcd = o3d.geometry.PointCloud()
+    pcd.points = o3d.utility.Vector3dVector(frame_in_np)
 
+    print("starting to display plot")
 
-o3d.visualization.draw_geometries([pcl_to_disp])
+    o3d.visualization.draw_geometries([pcd])
 
